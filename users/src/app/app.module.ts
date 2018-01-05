cc01a36f8daaa91d50239591e8298712cd467b70
sharanyadavgokul <sharangokul8088@gmail.com>
2018-01-05 19:43:33 +0530

    modified

--- users/src/app/app.module.ts
+++ users/src/app/app.module.ts
@@ -6,18 +6,18 @@ import { AppComponent } from './app.component';
 import { UsersInactiveComponent } from './users-inactive/users-inactive.component';
 import { UsersActiveComponent } from './users-active/users-active.component';
 import { UsersService } from './users.service';
-
+import {CounterService} from './counter.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersInactiveComponent,
-    UsersActiveComponent
+    UsersActiveComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [UsersService],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- users/src/app/users.service.ts
+++ users/src/app/users.service.ts
@@ -1,15 +1,21 @@
+import { Injectable } from '@angular/core';
 
+import { CounterService } from './counter.service';
+
+@Injectable()
 export class UsersService {
     activeUsers = ['sharan', 'gokul'];
     inactiveUsers = ['shiva', 'yadav'];
-
+    constructor(private counterService: CounterService) {}
     setToActive(id: number) {
         this.activeUsers.push(this.inactiveUsers[id]);
         this.inactiveUsers.splice(id, 1);
+        this.counterService.incrementInactiveToActive();
     }
 
     setToInactive(id: number) {
         this.inactiveUsers.push(this.activeUsers[id]);
         this.activeUsers.splice(id, 1);
+        this.counterService.incrementActiveToInactive();
     }
 }
