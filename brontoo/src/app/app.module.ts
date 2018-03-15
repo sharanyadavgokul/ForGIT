a711048d8f354446d1351415af39aaa030006634
sharanyadavgokul <sharangokul8088@gmail.com>
2018-03-15 13:09:08 +0530

    files modified

--- brontoo/src/app/app.module.ts
+++ brontoo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/Http';
 
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,13 @@ import { PreloginModule } from './prelogin/login/prelogin.module';
 import { HomeComponent } from './postlogin/home/home.component';
 import { LoginComponent } from './prelogin/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes: Routes = [
+  {path:'', redirectTo: '/login', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'home', component: HomeComponent},
+];
 
 
 @NgModule({
@@ -18,9 +25,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     PostloginModule,
     PreloginModule,
+    HttpModule,
     ReactiveFormsModule
   ],
   providers: [],
--- brontoo/src/app/prelogin/login/login.component.ts
+++ brontoo/src/app/prelogin/login/login.component.ts
@@ -1,24 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
+/* interface loginDetails{
+  email:string;
+  password:string
+} */
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor() { }
+  registerForm: FormGroup;
+   loginPage : Boolean = false;
+  constructor(private router: Router) { 
+
+  }
 
   ngOnInit() {
     this.loginForm= new FormGroup({
-      'loginid': new FormControl(null),
+      'loginid': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null),
+    });
+
+    this.registerForm= new FormGroup({
+      'loginid': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null),
     });
   }
+
   onSubmit(){
+    let loginid, password;
+    var testObject = {
+      "users": [
+          {
+          "email": "test1@gmail.com",
+          "password": "123456"
+        },
+        {
+          "email": "test2@gmail.com",
+          "password": "123456"
+        }
+      ]
+    };
     console.log(this.loginForm.value);
-    sessionStorage.setItem('loginid', this.loginForm.value.loginid)
-    sessionStorage.setItem('password', this.loginForm.value.password)
+    localStorage.setItem('authentication', JSON.stringify(testObject));
+    //localStorage.setItem('password', this.loginForm.value.password)
+    var authObj = JSON.parse(localStorage.getItem('authentication'));//testObject.users;
+    for(let i=0,j=authObj.users.length;i<j;i++){
+      if(authObj.users[i].email === this.loginForm.value.loginid && 
+        authObj.users[i].password === this.loginForm.value.password){
+        this.router.navigate(['/home']);
+      }
+    }
+    /* if(localStorage.getItem('loginid') === this.loginForm.value.loginid && 
+    localStorage.getItem('password') === this.loginForm.value.password){
+    this.router.navigate(['/home']);
+    } */
+  }
+
+  onRegister(){
+   //var register: loginDetails;
+   //register.email = this.registerForm.('loginid')
   }
 }
